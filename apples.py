29f5aced920a480ae5365f9fa344f07fb8cbd997
hemant <hemant@hemant.com>
2019-08-04 19:51:01 +0530

    final submission for apples.py

--- apples.py
+++ apples.py
@@ -1,6 +1,9 @@
 for i in range(int(input())):
     n, k = map(int, input().split())
-    if k == 1 or n == k*k:
+    if k == 1 or n%(k*k) == 0:
         print('NO')
     else:
         print('YES')
+
+
+        
